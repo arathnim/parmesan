d4d61a0da06c574d66f3de44bcff29744d02726f
arathnim <arathnim@gmail.com>
2017-09-24 03:12:04 -0500

    now with 100% more modularity

--- examples/sexp.cl
+++ examples/sexp.cl
@@ -1,31 +1,39 @@
-(ql:quickload '(parmesan))
-(in-package parmesan)
+;;; this file is meant to be run by itself
+
+(ql:quickload parmesan)
+(use-package 'parmesan)
 
 ;; Simple s-expr parser, meant only as an example.
 ;; Probably not even close to any kind of standard, but whatever.
 
 (defparser symbol-char () 
-  (one-of "~!@#$?%^&*-_=+<>,./\\"))
+   (one-of "~!@#$?%^&*-_=+<>,./\\"))
 
-(defparser whitespace ()
-	(choice #\Tab #\Space #\Newline))
+(defparser whitespace () 
+   (choice #\Tab #\Space #\Newline))
 
-(defparser digit () (range #\0 #\9))
-(defparser letter () (choice (range #\a #\z) (range #\A #\Z)))
+(defparser digit () 
+   (range #\0 #\9))
+
+(defparser letter () 
+   (choice (range #\a #\z) (range #\A #\Z)))
 
 (defparser between (start end parser)
-	(seq start (result <- parser) end
-		result))
+   start (result <- parser) end
+   result)
 
 (defparser ignore-whitespace (form) 
   (between (any whitespace) (any whitespace) form))
 
-;; according to the standard, symbols must start with either a letter or normal symbol characer, 
-;; and then any number of letters, symbol characters, or numbers
+;; according to the standard, symbols must start with either a letter or symbol characer, 
+;; and then any number of letters, symbol characters, or numbers.
+;; We don't actually have to do this though, since this parser is much more advanced
+;; than the character-based ones used in most lisps.
+
 (defparser parse-symbol ()
   (first-char <- (choice symbol-char letter))
   (other-char <- (any (choice symbol-char letter digit)))
-  (intern (string-upcase (cat first-char other-char))))
+  (intern (string-upcase (concatenate 'string (string first-char) other-char))))
 
 (defparser parse-int ()
   (n <- (many digit))
@@ -40,7 +48,3 @@
 
 (defparser expression ()
   (ignore-whitespace (choice parse-symbol parse-int parse-list parse-quote)))
-
-;; usage
-;; (parse-from-stream *standard-input* expression)
-;; (parse "(a b c)" expression)
--- parmesan.lisp
+++ parmesan.lisp
@@ -1,196 +1,103 @@
 ;; (author *this-code*) => ("Dylan Ball" "Arathnim@gmail.com")
 
 (proclaim '(optimize (speed 0) (safety 3) (debug 3) (space 0)))
-(declaim #+sbcl(sb-ext:muffle-conditions style-warning))
-(ql:quickload '(alexandria iterate anaphora destructuring-match) :silent t)
+
+(ql:quickload '(alexandria iterate wernicke) :silent t)
+
 (defpackage parmesan
-   (:use cl alexandria iterate anaphora destructuring-match))
+   (:use cl alexandria iterate)
+   (:import-from wernicke "ONE-OF" "STR" "CHR" "NONE-OF" "CHOICE"
+       "RANGE" "TRY" "OPTIONAL" "TIMES" "DEFINE-PARSER" "RUN-PARSER" "ERROR?")
+   (:export "DEFPARSER" "PARSE" "ONE-OF" "NONE-OF" 
+      "ANY" "MANY" "SEQ" "CHOICE" "RANGE" "TRY" "OPTIONAL" "TIMES"))
 
 (in-package parmesan)
 
-(defvar *source* nil)
-(defvar *index* 0)
-
-(defstruct (source)
-   value
-   (cached-string "")
-   (max-index -1))
-
-(defun get-character (n)
-   (if (stringp (source-value *source*))
-       (char (source-value *source*) n)
-       (if (> n (source-max-index *source*))
-           (iter (until (eql (source-max-index *source*) n))
-                 (append-character (read-char (source-value *source*)))
-                 (finally (return (char (source-cached-string *source*) n))))
-           (char (source-cached-string *source*) n))))
-
-(defun append-character (c)
-   (setf (source-cached-string *source*)
-         (concatenate 'string
-            (source-cached-string *source*)
-            (string c)))
-   (incf (source-max-index *source*)))
-
-;; error type and functions
-
-(declaim (inline error?))
-(defstruct (parser-error (:conc-name error-) (:predicate error?))
-   type index args)
-
-(declaim (inline pass))
-(defun pass (value n)
-   (incf *index* n)
-   value)
-
-(declaim (inline fail))
-(defun fail (type args &optional (index *index*))
-   (make-parser-error :type type :args args :index index))
-
-(defmacro on-success (exp &rest body)
-   (with-gensyms (r)
-      `(let ((,r ,exp))
-         (if (error? ,r)
-             ,r
-             (progn ,@body)))))
-
-(defmacro on-failure (exp &rest body)
-   (with-gensyms (r)
-      `(let ((,r ,exp))
-         (if (error? ,r)
-             (progn ,@body)
-             ,r))))
-
-;; 'interpreted' parsers, for dynamic calling at runtime
-;; used as a fallback and for prototyping new parsers
-
-(defparameter interpreted-parsers (make-hash-table :test #'equalp))
-(defparameter compiled-parsers (make-hash-table :test #'equalp))
-
-(defmacro define-parser (name ll &rest body)
-  `(setf (gethash ',name interpreted-parsers)
-         (lambda ,ll ,@body)))
-
-(defmacro define-compiled-parser (name ll &rest body)
-  `(setf (gethash ',name compiled-parsers)
-         (lambda ,ll ,@body)))
-
-(defun dynamic-run (exp)
-  (aif (and (listp exp) (gethash (car exp) interpreted-parsers))
-       (apply it (cdr exp))
-       (error "unexpected form: ~a" exp)))
-
-(defun compile-parser (exp)
-  (aif (and (listp exp) (gethash (car exp) compiled-parsers))
-       (apply it (cdr exp))
-       (error "unexpected form: ~a" exp)))
-
-(defmacro insert-compiled (exp)
-   (compile-parser exp))
-
-(defmacro parse (src form)
-  `(let ((*source* (make-source :value ,src))
-         (*index* 0))
-         (dynamic-run ',form)))
-
-(defmacro compiled-parse (src form)
-  `(let ((*source* (make-source :value ,src))
-         (*index* 0))
-         (insert-compiled ,form)))
-
-;; programmer options
-;;  backtracking
-;;  value-returning
-;;  errors
-
-(defvar *settings* '(:backtracking nil :value-returning t :errors t))
-
-;; choice   ~ in the matching operation, returns the first form that consumes input
-;; any      ~ matches zero or more of the next form
-;; many     ~ matches one or more of the next form
-;; one-of   ~ matches one character from the given string
-;; none-of  ~ matches only if none of the chars in the string match
-;; times    ~ parses x occurances of y
-;; try      ~ returns a parsing result, no effect on the normal parser stack
-;; optional ~ tries x, parses it on success, succeeds either way
-;; seq      ~ matches each form sequentially, returns a list of forms or nil
-;; str      ~ explicitly matches a string
-;; sep      ~ seperates by repeated matching. mostly magic
-
-(declaim (inline test-remaining))
-(defun test-remaining (n)
-   (if (stringp (source-value *source*))
-       (>= (length (source-value *source*)) (+ *index* n))
-       t))
-
-;; char
-
-(define-parser chr (char)
-   (if (test-remaining 1)
-       (if (eql (get-character *index*) char)
-           (pass char 1)
-           (fail (string char) nil))
-       (fail (string char) nil)))
-
-(define-compiled-parser chr (char)
-  (once-only (char)
-   `(if (test-remaining 1)
-        (if (eql (get-character *index*) ,char)
-            (pass ,char 1)
-            (fail (string ,char) nil))
-        (fail (string ,char) nil))))
-
-;; str
-
-(define-parser str (string)
-   (iter (for char in-string string)
-         (for parse-result = (dynamic-run `(chr ,char)))
-         (on-failure parse-result (leave parse-result))
-         (finally (return string))))
-
-(define-compiled-parser str (string)
-   (once-only (string)
-     `(iter (for char in-string ,string)
-            (for parse-result = ,(compile-parser '(chr char)))
-            (on-failure parse-result (leave parse-result))
-            (finally (return ,string)))))
-
-;; try
-
-(define-parser try (p)
-   (let* ((*index* *index*))
-      (dynamic-run p)))
-
-;; any
-
-(define-parser any (p)
-   (iter (for r = (dynamic-run p))
-         (until (error? r))
-         (collect r into result)
-         (finally (return (merge-characters result)))))
-
-;; many
+;; compressing
 
 (define-parser many (p)
-   (iter (for r = (dynamic-run p))
-         (until (error? r))
-         (collect r into result)
-         (finally (return (if result (merge-characters result) (fail 'many nil))))))
+   (let ((result (run-parser (wernicke:many p))))
+      (if (and (not (error? result)) (every #'characterp result)) 
+          (coerce result 'string) 
+          result)))
 
-(define-parser choice (&rest parsers)
-   (iter (for parser in parsers)
-         (for parser-result = (dynamic-run parser))
-         (on-success parser-result (leave parser-result))
-         (finally (return (fail 'choice nil)))))
+(define-parser any (p)
+   (let ((result (run-parser (wernicke:any p))))
+      (if (and (not (error? result)) (every #'characterp result)) 
+          (coerce result 'string) 
+          result)))
 
-(define-parser times (n parser)
-   (iter (repeat n)
-         (for parse-result = (dynamic-run parser))
-         (on-failure parse-result (leave parse-result))
-         (collect parse-result into list)
-         (finally (return (merge-characters list)))))
+(define-parser seq (&rest p)
+   (let ((result (run-parser (apply #'wernicke:seq p))))
+      (if (and (not (error? result)) (eql (length p) 1)) 
+          (car result) 
+          result)))
 
-(define-parser optional (parser)
-   (if (error? (dynamic-run (try parser)))
-       nil
-       (run parser)))
+;; shorthand for characters and strings
+
+(defmacro parse (string form)
+   `(let ((wernicke:*run-hooks* 
+            (list (lambda (p) (if (stringp p) (wernicke::str p) p))
+                  (lambda (p) (if (characterp p) (wernicke::chr p) p)))))
+         (wernicke:parse ,string ,form)))
+
+;; variable assignment
+
+(defun symbol-eq (x y)
+   (and (symbolp x) (symbolp y) (string= (string x) (string y))))
+
+(defun generate-single-binding (var form)
+  `(lambda () (setf ,var (run-parser ,form)) ,var))
+
+(defun generate-multiple-binding (vars form)
+   (with-gensyms (parse-result)
+     `(lambda ()
+        (let ((,parse-result (run-parser ,form))) 
+          (if (listp ,parse-result) 
+              (progn
+                (when (not (eql (length ,parse-result) ,(length vars)))
+                      (error "result of ~a is the wrong length for the variable list ~a" ',form '(,@vars)))
+                ,@(iter (for x in vars)
+                        (for y upfrom 0) 
+                        (collect `(setf ,x (nth ,y ,parse-result)))))
+              ,parse-result)))))
+
+(defun extract-binding (exp)
+   (when (and (listp exp) (member '<- exp :test #'symbol-eq))
+         (subseq exp 0 (position '<- exp :test #'symbol-eq))))
+
+(defun without-binding (exp)
+   (car (subseq exp (+ 1 (position '<- exp :test #'symbol-eq)))))
+
+(defun transform-binds (forms)
+   (iter (for exp in forms)
+         (for vars = (extract-binding exp))
+         (when vars (appending vars into free-variables))
+         (collect
+            (if vars
+               (if (> (length vars) 1)
+                   (generate-multiple-binding vars (without-binding exp))
+                   (generate-single-binding (car vars) (without-binding exp)))
+               exp)
+            into new-forms)
+         (finally (return (list free-variables new-forms)))))
+
+(defun binding-seq (forms)
+   (let* ((result (transform-binds forms))
+          (vars (first result))
+          (new-forms (second result)))
+          (if vars
+             (with-gensyms (r)
+               `(let (,@vars) 
+                 (let ((,r (run-parser (seq ,@(butlast new-forms)))))
+                       (if (error? ,r)
+                           ,r
+                           ,(car (last new-forms))))))
+             `(wernicke::run-parser (seq ,@new-forms)))))
+
+;; no-args calling shorthand
+
+(defmacro defparser (name ll &rest body)
+   `(progn (define-parser ,name ,ll ,(binding-seq body))
+          ,(unless ll `(defparameter ,name (funcall (function ,name))))))
+
